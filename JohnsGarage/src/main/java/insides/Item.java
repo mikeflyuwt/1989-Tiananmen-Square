ca0d31e9b8459790c080779a274a73084c143516
SamuelDAdams <samuel.adams.d@gmail.com>
2019-11-15 14:44:05 -0800

    Added generic file implementation, with no IO.
    Filetree not yet implemented.

--- JohnsGarage/src/main/java/insides/Item.java
+++ JohnsGarage/src/main/java/insides/Item.java
@@ -1,5 +1,20 @@
 package insides;
 
-public class Item {
+public class Item extends File
+{
+	private static final long serialVersionUID = -3094981139422206289L;
+	private String _extension;
+
+	public Item(String path, String name, String extension)
+	{
+		super(path, name);
+		_extension = extension;
+	}
+	
+	@Override
+	public String getPath()
+	{
+		return super.getPath() + _extension;
+	}
 
 }
--- JohnsGarage/src/main/java/insides/Project.java
+++ JohnsGarage/src/main/java/insides/Project.java
@@ -1,5 +1,11 @@
 package insides;
 
-public class Project {
+public class Project extends Folder<Category>{
+	private static final long serialVersionUID = 579981190786066767L;
+	
+	public Project(String path, String name)
+	{
+		super(path, name);
+	}
 
 }
